07332226bfc0d6e6602674c0811dc78dfd663386
vlado78 <vladimir.budac@gmail.com>
2018-07-26 17:15:00 +0200

    google maps

--- Servis/kontakt.php
+++ Servis/kontakt.php
@@ -13,10 +13,26 @@
     <div class="grid-x">
         <div class="cell large-6">
          mapa
-
+         
+         <div id="map" style="width:400px;height:400px;background:yellow"></div>
+         <script>
+          function myMap() {
+              var mapOptions = {
+                  center: new google.maps.LatLng(51.5, -0.12),
+                  zoom: 10,
+                  mapTypeId: google.maps.MapTypeId.HYBRID
+              }
+          var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+          }
+        </script>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
         </div>
+
+
         <div class="cell large-6">
-        kontakt
+        Adresa
+        Telefon
+        Radno vrijeme
         </div>
     </div>
 

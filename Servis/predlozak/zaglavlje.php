7e75a55e197fddcf077cdd653272c90301340d8c
vlado78 <vladimir.budac@gmail.com>
2018-08-03 19:32:02 +0200

    svg

--- Servis/predlozak/zaglavlje.php
+++ Servis/predlozak/zaglavlje.php
@@ -3,7 +3,9 @@
         <div class="callout">
             <div class="grid-x grid-padding-x">
                 <div class="large-2 medium-4 cell">
-                moj logo
+                
+                <svg id="logo" height="60" width="100">
+                    <image x="0" y="0"  height="60" width="100"   xlink:href="<?php echo $putanjaAPP ?>img/logo.svg" />
                 </div>
                <div class="large-10 medium-8 cell text-right">
                     <h1><?php echo $nazivAPP; ?></h1>
